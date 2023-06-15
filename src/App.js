e49cc7c90b1157f0794bb81ccac129db24107c12
Salman Shaikh <sk.salman0425@gmail.com>
2023-06-15 18:02:12 +0530

    basic folder structure added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { addToCart } from './redux/action';
+import { useDispatch } from 'react-redux'
+
+function App() {
+  const dispatch = useDispatch();
+  const product = {
+    name: 'i Phone',
+    category: 'mobile',
+    price: 10000,
+    color: 'red'
+  }
+  return (
+    <div className="App">
+      <button onClick={() => dispatch(addToCart(product))}>Add To Cart</button>
+    </div>
+  );
+}
+
+export default App;
