e47c0d311d377b5c5eb875b8dbec4fcd7e84cfce
Salman Shaikh <sk.salman0425@gmail.com>
2023-06-16 18:18:03 +0530

    remove and empty cart functionality added

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,9 +1,24 @@
-import { ADD_TO_CART } from "./constant"
+import { ADD_TO_CART,EMPTY_CART,REMOVE_FROM_CART } from "./constant"
 
 export const addToCart = (data) => {
-    console.warn("action is called", data)
+    console.warn("action addToCart is called", data)
     return {
         type: ADD_TO_CART,
         data:data
+    }
+}
+export const removeFromCart = (data) => {
+    console.warn("action removeFromCart is called", data)
+    return {
+        type: REMOVE_FROM_CART,
+        data:data
+    }
+}
+//for emptying cart we don't need data
+export const emptyCart = () => {
+    console.warn("action emptyCart is called")
+    return {
+        type: EMPTY_CART,
+      
     }
 }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,4 @@
-import { ADD_TO_CART } from "./constant"
-
-
-
+import { ADD_TO_CART, EMPTY_CART, REMOVE_FROM_CART } from "./constant"
 // export const cartData = (data = [], action) => {
 //     if (action.type === ADD_TO_CART) {
 //     console.warn("ADD_TO_CART condtion ", action)
@@ -12,15 +9,19 @@ import { ADD_TO_CART } from "./constant"
 //         return "no action called"
 //     }
 // }
-
-
-
-
 export const cartData = (data = [], action) => {
     switch (action.type) {
         case ADD_TO_CART:
             console.warn("ADD_TO_CART condition ", action)
             return [action.data, ...data]
+        case REMOVE_FROM_CART:
+                console.warn("REMOVE_FROM_CART condition ", action)
+                data.length=data.length?data.length-1:[];//if data.length is zero than return []
+                return [...data]    
+        case EMPTY_CART:
+                console.warn("EMPTY_CART condition ", action)
+                data=[]
+                return [...data] 
         default:
             return []
     }
