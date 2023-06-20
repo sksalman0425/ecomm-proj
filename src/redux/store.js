d27d39fc6e711e2bf54dac1d15f0561f56f26ff6
Salman Shaikh <sk.salman0425@gmail.com>
2023-06-20 15:25:57 +0530

    latest changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore } from "redux";
+import { createStore } from "redux";//createStore function is depreciated that's why cross line coming
 import rootReducer from "./rootReducer";
 
 
